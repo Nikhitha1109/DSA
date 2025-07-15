0f61ed98f633cd3fc2a65a86ac342926fc93968c
Nikhitha1109 <pothulanikhitha@gmail.com>
2025-07-15 10:36:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (78.57%) - LeetSync

--- 3396-valid-word/valid-word.py
+++ 3396-valid-word/valid-word.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        if len(word) <3:
+        VOWELS = "aeiou"
+
+        if len(word) < 3:
             return False
-        vowels=set("aeiouAEIOU")
-        vowel_found=False
-        con_found=False
-        for ch in word:
-            if not (ch.isalpha() or ch.isdigit()):
-                return False
-            if(ch.isalpha()):
-                if ch in vowels:
-                    vowel_found=True
+        vowel = consonant = False
+        for x in word:
+            if x.isalpha():
+                if x.lower() in VOWELS:
+                    vowel = True
                 else:
-                    con_found=True
-        return vowel_found and con_found
+                    consonant = True
+            elif not x.isdigit():
+                return False
+        return vowel and consonant
