1cdf07c278b65c34badec54088dae6d11e19580d
Nikhitha1109 <pothulanikhitha@gmail.com>
2025-07-15 10:11:52 +0530

    Time: 2 ms (92.39%) | Memory: 41.2 MB (88.57%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,24 +1,28 @@
+
 class Solution {
     public String countAndSay(int n) {
-        String term = "1";
-        
-        for (int i = 2; i <= n; i++) {
-            StringBuilder nextTerm = new StringBuilder();
-            int count = 1;
-            
-            for (int j = 1; j < term.length(); j++) {
-                if (term.charAt(j) == term.charAt(j - 1)) {
-                    count++;
-                } else {
-                    nextTerm.append(count).append(term.charAt(j - 1));
-                    count = 1;
-                }
-            }
-            
-            nextTerm.append(count).append(term.charAt(term.length() - 1));
-            term = nextTerm.toString();
+        String s = "1";
+        for (int i=1; i<n; i++) {
+            s = countAndSayHelper(s);
         }
-        
-        return term;
+        return s;
+    }
+    public String  countAndSayHelper(String s) {
+        char previous = s.charAt(0);
+        int count = 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i=1; i<s.length(); i++) {
+            if (s.charAt(i) == previous) {
+                count++;
+            } else {
+                sb.append(count);
+                sb.append(previous);
+                previous = s.charAt(i);
+                count = 1;
+            }
+        }
+        sb.append(count);
+        sb.append(previous);
+        return sb.toString();
     }
 }
