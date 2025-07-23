5c0b8f33ca5815834f32755bc05926979e576748
Nikhitha1109 <pothulanikhitha@gmail.com>
2025-07-23 21:50:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (89.68%) - LeetSync

---
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1;
+        int total = 10; 
+        int uniqueDigits = 9;
+        int available = 9;
+        for (int i = 2; i <= n; i++) {
+            uniqueDigits *= available;
+            total += uniqueDigits;
+            available--;
+        }
+        return total;
+    }
+}
