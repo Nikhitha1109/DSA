6575969635769de687d18c2541242e3740a4c833
Pothula Nikhitha <pothulanikhitha@gmail.com>
2026-01-20 11:20:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (88.40%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,7 +1,13 @@
-public class Solution {
+class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim(); 
-        int lastSpaceIndex = s.lastIndexOf(' ');
-        return s.length() - lastSpaceIndex - 1;
+        String str=s.trim(); //extra spaces removed using bulitin functon of string in java
+        int c=0;
+        for(int i=str.length()-1;i>=0;i--){
+            if(str.charAt(i)!=' '){
+                c++;
+            }else{
+                break;
+            }
+        }return c;
     }
 }
