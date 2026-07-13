8f3c1eedc8fff8e05a97fe360868faaa1ea8a93e
Pothula Nikhitha <pothulanikhitha@gmail.com>
2026-07-13 10:06:53 +0530

    Time: 10 ms (26.48%) | Memory: 42 MB (52.16%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,16 +1,7 @@
 class Solution {
-
-    public int fib (int n){
-        if(n<=1){
-            return n;
-        }
-       int [] map =new int[n+1];
-        map[1]=1;
-       for(int i=2;i<=n;i++){
-            int a=map[i-1];
-            int b=map[i-2];
-            map[i]=a+b;
-       }
-       return map[n];
+    public int fib(int n) {
+        if(n==1) return 1;
+        if(n<=0) return 0;
+        return fib(n-1)+fib(n-2);
     }
 }
